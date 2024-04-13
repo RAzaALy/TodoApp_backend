7643a1b6f1c523a69bea41b41c099c746e003cb6
RazaAly <007razajutt@gmail.com>
2024-04-13 23:51:32 +0500

    cors refactor

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-import bodyParser from 'body-parser'
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const connectToDb = require("./config/db.confg.js");
 const userRouter = require("./route/userRoute.js");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   helmet({
@@ -18,8 +19,15 @@ app.use(
     frameguard: true,
   })
 );
-app.use(cors());
-// app.use(cors({origin: ['http://localhost:5173','https://todo-app-frontend-pearl.vercel.app'], credentials: true}))
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["https://todo-app-frontend-pearl.vercel.app","http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use("/", userRouter);
 
